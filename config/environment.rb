b6a554ec910ef2cbf3fe2dc25ea072afc48b0961
Eugene Noble <nobleeugene2005@gmail.com>
2021-09-27 20:04:53 +0100

    new commit

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require_relative "application"
-
+env_file = File.join(Rails.root,"config","local_env.rb")
+load(env_file) if File.exist?(env_file)
 # Initialize the Rails application.
 Rails.application.initialize!
